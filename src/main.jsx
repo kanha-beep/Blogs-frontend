29fa1c75444b176124e2f64dc4109b4c8644ee81
kanha-beep <kanhashree2223@gmail.com>
2026-01-03 19:22:08 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.css';
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
